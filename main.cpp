11f69024a4a31094242dfee8cfd071a77b132034
Nathan <163361000+njmoon21@users.noreply.github.com>
2025-11-28 22:01:07 -0800

    Implemented lwHit() and lwMiss()
    Created rough implementation code for functions lwHit() and lwMiss(), also known as read hit and read miss. Additionally changed 'input_object_code' file into a .txt file and moved it into the cmake-build-debug directory.
    
    Currently Incompleted:
    - swHit() & swMiss()
    - Print functions

--- main.cpp
+++ main.cpp
@@ -36,8 +36,50 @@ struct Block {
      */
 
     int valid = 0;
-    int tag, data;
+    int tag, data, history;
 };
+
+void lwHit(Block &accessedBlock, Block &otherBlock, int &rt_register) { // Cache data -> reg.file[rt]
+    rt_register = accessedBlock.data;
+    accessedBlock.history = 1;
+    otherBlock.history = 0;
+    std::cout << "hit\n";
+}
+
+void lwMiss(Block &block0, Block &block1, int tag, int index, int &rt_register, int* main_memory) {
+    /*
+     * 1. Select victim block from 2 blocks;
+     * 2. If victim's valid bit == 1, update memory;
+     * 3. Bring (copy) memory block to victim block;
+     * 4. Set valid bit = 1, and tag field;
+     * 5. reg.file[rt] <-- cache block data;
+     */
+
+    Block victim_block;
+    if (block1.history == 1)
+        victim_block = block0;
+    else
+        victim_block = block1;
+
+    if (victim_block.valid == 1) {
+        main_memory[tag * 8 + index] = victim_block.data;
+    }
+
+    // Bring copy of memory block to victim block
+    victim_block.data = main_memory[index];
+
+    victim_block.valid = 1;
+    victim_block.tag = tag;
+    rt_register = victim_block.data;
+}
+
+void swHit() { // reg.file[rt] --> cache data; (update cache only) WB cache
+
+}
+
+void swMiss() { // reg.file[rt] --> memory[word_addr]; (update memory only)
+
+}
 
 int main() {
     /*
@@ -51,45 +93,80 @@ int main() {
      *   - determine byte_offset value;
      * 3. Compute effective memory address (byte address) and convert it to a word address;
      * 4. From the word address, compute cache index and tag values using MOD and DIV;
-     * 5. Search cache and do read/write operation, i.e., one of
+     * 5. Search cache and do read/write operation, i.e., one of TODO
      *   {read_hit, read_miss, write_hit, write_miss};
      * 6. repeat step 1~5 for each instruction in the input data file (while loop until EOF).
      */
 
-    std::ifstream file("input_object_code");
     int register_file[8]; // 8 register files
     Block cache[8][2]; // 2-way set associative cache
     int main_memory[128]; // 128 main memory blocks, 1 word per block
+    std::ifstream file("input_object_code.txt");
 
-    while (!file.eof()){
-        std::string s;
-        int opcode;
-        int register_rt = 0;
-        int offset = 0;
+    if (file.is_open()) {
+        while (!file.eof()){
+            std::string s;
+            int opcode;
+            int rt_register = 0;
+            int offset = 0;
 
-        // Fetching line instruction
-        std::getline(file, s);
 
-        // Decoding
-        if (s.substr(0, 6) == "100011")
-            opcode = 35; // lw instr.
-        else if (s.substr(0, 6) == "101010")
-            opcode = 43; // sw instr.
+            // Fetching line instruction
+            std::getline(file, s);
 
-        // $rs will always be zero
-        // Decoding register_rt
-        for (int i = 15; i >= 11; i--) {
-            if (s.at(i) == '1')
-                register_rt += pow(2, 15-i);
+            // Decoding
+            if (s.substr(0, 6) == "100011")
+                opcode = 35; // lw instr.
+            else if (s.substr(0, 6) == "101011")
+                opcode = 43; // sw instr.
+
+            // $rs will always be zero
+            // Decoding register_rt
+            for (int i = 15; i >= 11; i--) {
+                if (s[i] == '1')
+                    rt_register += pow(2, 15-i);
+            }
+
+            // Decoding offset
+            for (int i = 31; i > 15; i--) {
+                if (s[i] == '1')
+                    offset += pow(2, 31-i);
+            }
+
+            // Calculating physical address and then both the tag and index for the cache
+            int physical_address = offset / 4; // Converting from byte to word address
+            int tag = physical_address / 8;
+            int index = physical_address % 8;
+
+            // Based on the index, we are going to check this set in our cache
+            Block& set_block0 = cache[index][0];
+            Block& set_block1 = cache[index][1];
+
+            // Checking if there is valid data in one of the blocks, as well as matching tags
+            bool hit_on_block0 = set_block0.valid && set_block0.tag == tag;
+            bool hit_on_block1 = set_block1.valid && set_block1.tag == tag;
+
+            if (opcode == 35) {
+                if (hit_on_block0 == true)
+                    lwHit(set_block0, set_block1, rt_register);
+                else if (hit_on_block1 == true)
+                    lwHit(set_block1, set_block0, rt_register);
+                else // hit == false
+                    lwMiss(set_block0, set_block1, tag, index, rt_register, main_memory);
+            }
+
+            if (opcode == 43) {
+                if (hit_on_block0 == true)
+                    swHit();
+                else if (hit_on_block1 == true)
+                    swHit();
+                else
+                    swMiss();
+            }
         }
-
-        // Decoding offset
-        for (int i = 31; i > 15; i--) {
-            if (s.at(i) == '1')
-                offset += pow(2, 31-i);
-        }
-        offset /= 4;  // Converting from byte to word address
     }
+    else
+        std::cout << "Unable to open file\n";
 
     file.close();
     return 0;
